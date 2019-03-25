6c12ffd5c326723f152a80d0c6fa3f0a37f546df
Noel Ryan <noelgerard.ryan@gmail.com>
2019-03-25 14:47:20 +0100

    created the bmi calculator, added the metric and imperial methods

---
+++ src/js/bmi_calculator.js
@@ -0,0 +1,42 @@
+function BMICalculator() {
+    this.metric_bmi = function (object) {
+        const weight = object.weight;
+        const height = object.height;
+        if (weight > 0 && height > 0) {
+            const finalBmi = weight / (height / 100 * height / 100);
+            object.bmiValue = parseFloat(finalBmi.toFixed(2));
+            this.setBMIMessage(object)
+        }
+    }
+
+    this.imperial_bmi = function(obj) {
+        let weight = obj.weight;
+        let height = obj.height;
+        if (weight > 0 && height > 0) {
+          let finalBmi = weight / (height * height) * 703;
+              obj.bmiValue = parseFloat(finalBmi.toFixed(2));
+              setBMIMessage(obj);
+        }
+    }
+
+    this.setBMIMessage = function(object) {
+        if (object.bmiValue < 18.5) {
+            object.bmiMessage = "Underweight"
+        }
+        if (object.bmiValue > 18.5 && object.bmiValue < 25) {
+            object.bmiMessage = "Healthy"
+        }
+        if (object.bmiValue > 25 && object.bmiValue < 30) {
+            object.bmiMessage = "Overweight"
+        }
+        if (object.bmiValue > 30) {
+            object.bmiMessage = "Obese"
+        }
+    }
+}
+
+
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = BMICalculator;
+}
---
+++ src/js/person.js
@@ -0,0 +1,17 @@
+function Person(attrs) {
+    this.weight = attrs.weight;
+    this.height = attrs.height;
+    this.calculate_bmi = function () {
+        const calculator = new BMICalculator();
+        calculator.metric_bmi(this)
+    }
+
+    this.calculate_imperial_bmi  = function() {
+        calculator = new BMICalculator();
+        calculator.imperial_bmi(this);
+    }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Person;
+}
